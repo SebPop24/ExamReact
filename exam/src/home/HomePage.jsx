37ff6099462aa23d7a6a3f386b5a9efad6b6e825
SebPop24 <sebbe2405@gmail.com>
2024-11-26 19:17:48 +0100

    Fixed carousel
    Fixed carousel

--- exam/src/home/HomePage.jsx
+++ exam/src/home/HomePage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
+
+const API_URL = 'http://localhost:5063'
 
 const HomePage = () => {
   return (
     <div className="text-center">
-        <h1 className="display-4">Welcome to My Shop</h1>
+        <h1 className="display-4">Your Nutrition
+        Journey Starts Here!</h1>
+      <Carousel>
+        <Carousel.Item>
+          <Image src={`${API_URL}/images/nøkkelhull.png`} className="d-block w-100" alt="Pizza" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src={`${API_URL}/images/salad.png`} className="d-block w-100" alt="Fish and Chips" />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
